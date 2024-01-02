8c2b1a4fc2a727a8acb825b00d0cba05f7f687a2
Michael Bisgaard Olesen <mo@concordium.com>
2024-01-02 15:51:12 +0100

    Integrate legacy crypto libraries
    Added dependency to the library `ConcordiumWalletCrypto` and added tests for invoking the `parameter_to_json` function. The library is currently used in the iOS reference wallet.
    
    This is only meant as a POC for integrating a Rust library that's already known to work. The dependency will be replaced with the new Rust library that's currently being built.

--- Package.swift
+++ Package.swift
@@ -1,23 +1,30 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "concordium-swift-sdk",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "concordium-swift-sdk",
-            targets: ["concordium-swift-sdk"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "concordium-swift-sdk"),
-        .testTarget(
-            name: "concordium-swift-sdkTests",
-            dependencies: ["concordium-swift-sdk"]),
-    ]
+        name: "concordium-swift-sdk",
+        products: [
+            .library(
+                    name: "concordium-swift-sdk",
+                    targets: ["concordium-swift-sdk"]),
+        ],
+        dependencies: [
+            .package(url: "https://github.com/Concordium/concordium-wallet-crypto-swift", exact: "0.24.0-0"),
+        ],
+        targets: [
+            .target(
+                    name: "concordium-swift-sdk",
+                    dependencies: [
+                        .product(name: "ConcordiumWalletCrypto", package: "concordium-wallet-crypto-swift"),
+                    ]
+            ),
+            .testTarget(
+                    name: "concordium-swift-sdkTests",
+                    dependencies: [
+                        "concordium-swift-sdk",
+                    ]
+            ),
+        ]
 )
--- Sources/concordium-swift-sdk/concordium_swift_sdk.swift
+++ Sources/concordium-swift-sdk/concordium_swift_sdk.swift
@@ -1,2 +1,24 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import MobileWallet
+
+enum MobileWalletError: Error, Equatable {
+    case noResponse
+    case failed(String)
+}
+
+func parameterToJson(_ inputJson: String) throws -> String {
+    // From https://github.com/Concordium/concordium-reference-wallet-ios/blob/24a7ed0ac635bd56aac89f536fff0739b12cd577/Dependencies/MobileWalletFacade.swift#L127
+    var res = ""
+    try inputJson.withCString { inputPointer in
+        var code: UInt8 = 0
+        guard let resPtr = parameter_to_json(inputPointer, &code) else {
+            throw MobileWalletError.noResponse
+        }
+        res = String(cString: resPtr)
+        free_response_string(resPtr)
+
+        guard code == 1 else {
+            throw MobileWalletError.failed(res)
+        }
+    }
+    return res
+}
