2b54880c59133d023de9b6635c996d591a4dfa34
Michael Bisgaard Olesen <mo@concordium.com>
2024-01-05 12:29:06 +0100

    Add example "test" for running a gRPC query against at real node

---
+++ Tests/concordium-swift-sdkTests/Example/concordium-swift-sdkExampleTests.swift
@@ -0,0 +1,38 @@
+@testable import concordium_swift_sdk
+import XCTest
+
+import GRPC
+import NIOCore
+import NIOPosix
+
+/// Temporary test for exercising the gRPC client. To run, adjust the channel target to point to a running node.
+final class concordium_swift_sdkExampleTests: XCTestCase {
+    let channelTarget = ConnectionTarget.host("localhost", port: 20000)
+    
+    func testExampleClientConsensusStatus() throws {
+        // Setup an `EventLoopGroup` for the connection to run on.
+        //
+        // See: https://github.com/apple/swift-nio#eventloops-and-eventloopgroups
+        let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        // Make sure the group is shutdown when we're done with it.
+        defer {
+          try! group.syncShutdownGracefully()
+        }
+        let channel = try GRPCChannelPool.with(
+            target: channelTarget,
+            transportSecurity: .plaintext,
+            eventLoopGroup: group
+        )
+        // Close the connection when we're done with it.
+        defer {
+          try! channel.close().wait()
+        }
+        let client = Concordium_V2_QueriesNIOClient(channel: channel)
+        
+        let req = Concordium_V2_Empty()
+        let res = client.getNodeInfo(req)
+        let info = try res.response.wait()
+        print(info)
+        XCTAssertNotNil(info)
+    }
+}
