3e3338b6e6470129a9cd73c6d18c0f2b178a045a
Michael Bisgaard Olesen <mo@concordium.com>
2024-01-24 10:53:27 +0100

    Introduce protocol for client and rename target (#12)

--- Package.swift
+++ Package.swift
@@ -3,36 +3,37 @@
 import PackageDescription
 
 let package = Package(
-        name: "ConcordiumSwiftSDK",
-        platforms: [
-            // To be kept in sync with README.
-            .iOS(.v15),
-            .macOS(.v10_15),
-        ],
-        products: [
-            .library(
-                    name: "ConcordiumSwiftSDK",
-                    targets: ["ConcordiumSwiftSDK"]),
-        ],
-        dependencies: [
-            .package(url: "https://github.com/anquii/Base58Check.git", from: "1.0.0"),
-            .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.15.0"),
-            .package(url: "https://github.com/nicklockwood/SwiftFormat.git", exact: "0.53.0"),
-        ],
-        targets: [
-            .target(
-                    name: "ConcordiumSwiftSDK",
-                    dependencies: [
-                        "Base58Check",
-                        .product(name: "GRPC", package: "grpc-swift"),
-                        "SwiftFormat",
-                    ]
-            ),
-            .testTarget(
-                    name: "ConcordiumSwiftSDKTests",
-                    dependencies: [
-                        "ConcordiumSwiftSDK",
-                    ]
-            ),
-        ]
+    name: "ConcordiumSwiftSdk",
+    platforms: [
+        // To be kept in sync with README.
+        .iOS(.v15),
+        .macOS(.v10_15),
+    ],
+    products: [
+        .library(
+            name: "ConcordiumSwiftSdk",
+            targets: ["ConcordiumSwiftSdk"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/anquii/Base58Check.git", from: "1.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.15.0"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat.git", exact: "0.53.0"),
+    ],
+    targets: [
+        .target(
+            name: "ConcordiumSwiftSdk",
+            dependencies: [
+                "Base58Check",
+                .product(name: "GRPC", package: "grpc-swift"),
+                "SwiftFormat",
+            ]
+        ),
+        .testTarget(
+            name: "ConcordiumSwiftSdkTests",
+            dependencies: [
+                "ConcordiumSwiftSdk",
+            ]
+        ),
+    ]
 )
