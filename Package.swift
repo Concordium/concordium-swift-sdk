5e700261a0dc08dd483981ce75c008d6bc3c80e5
Michael Bisgaard Olesen <mo@concordium.com>
2024-02-06 13:37:08 +0100

    Migrate 'crypto' (Rust bindings) to separate repo (#16)
    ## Purpose
    
    Complete the migration of the Rust binding library, i.e. `ConcordiumWalletCrypto` which was imported into another repository in https://github.com/Concordium/concordium-wallet-crypto-swift/pull/2.
    
    ## Changes
    
    Replace the previously embedded Rust code with an external dependency to the repository that it was moved to.

--- Package.swift
+++ Package.swift
@@ -19,14 +19,14 @@ let package = Package(
         .package(url: "https://github.com/anquii/Base58Check.git", from: "1.0.0"),
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.15.0"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat.git", exact: "0.53.0"),
-        .package(path: "./lib/crypto/ConcordiumWalletCrypto"),
+        .package(url: "https://github.com/Concordium/concordium-wallet-crypto-swift.git", exact: "1.0.0"),
     ],
     targets: [
         .target(
             name: "ConcordiumSwiftSdk",
             dependencies: [
                 "Base58Check",
-                "ConcordiumWalletCrypto",
+                .product(name: "ConcordiumWalletCrypto", package: "concordium-wallet-crypto-swift"),
                 .product(name: "GRPC", package: "grpc-swift"),
                 "SwiftFormat",
             ]
