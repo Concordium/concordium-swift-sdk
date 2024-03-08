9338f468f5df1ca7ae4549a99eb2836a2b6b833d
Michael Bisgaard Olesen <mo@concordium.com>
2024-03-08 09:53:33 +0100

    Minor cleanup

--- Sources/ConcordiumSwiftSdk/Error.swift
+++ Sources/ConcordiumSwiftSdk/Error.swift
@@ -2,13 +2,13 @@ import Foundation
 
 infix operator ?!: NilCoalescingPrecedence
 
-/// Expect the value of the left hand side to be not `nil` by throwing  the error on the right hand if it isn't.
+/// Expect the value of the left hand side to be not `nil` by throwing the error on the right hand if it isn't.
 ///
 /// For example, expression
 /// ```
 /// computeSomething() ?! MyError.computationFailed
 /// ```
-/// evaluates to the result of `computeSomething()`unless that result is `nil`
+/// evaluates to the result of `computeSomething()` unless that result is `nil`
 /// in which case it throws the custom error `MyError.computationFailed`.
 func ?! <T>(value: T?, error: @autoclosure () -> Error) throws -> T {
     guard let value else {
