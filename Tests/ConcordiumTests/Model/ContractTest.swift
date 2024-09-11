460695f415fc0b3bfb15016783f0a0855b046cff
Søren Bruus Zeppelin <soerenbf@gmail.com>
2024-09-11 16:05:18 +0200

    Unit tests for schema interactions

--- Tests/ConcordiumTests/Model/ContractTest.swift
+++ Tests/ConcordiumTests/Model/ContractTest.swift
@@ -40,13 +40,47 @@ final class ContractTest: XCTestCase {
         XCTAssertEqual(try schema.template, expected)
     }
 
-    func testTypeSchemaConvert() throws {
+    func testSchemaEq() throws {
+        let moduleSchema = try ModuleSchema(base64: TEST_CONTRACT_SCHEMA)
+
+        var value = try moduleSchema.initErrorSchema(contractName: ContractName("TestContract"))
+        var expected = try TypeSchema(base64: TEST_CONTRACT_INIT_ERROR_SCHEMA)
+        XCTAssertEqual(value, expected)
+
+        value = try moduleSchema.receiveErrorSchema(receiveName: ReceiveName(contractName: ContractName("TestContract"), entrypoint: EntrypointName("receive_function")))
+        expected = try TypeSchema(base64: TEST_CONTRACT_RECEIVE_ERROR_SCHEMA)
+        XCTAssertEqual(value, expected)
+    }
+
+    func testSimpleTypeSchemaConvert() throws {
         var schema = try TypeSchema(base64: TEST_CONTRACT_INIT_ERROR_SCHEMA)
-        let u = try schema.decode(data: Data(hex: "ffff")).parse(UInt16.self)
+        var u = try schema.decode(data: Data(hex: "ffff")).parse(UInt16.self)
         XCTAssertEqual(u, 65535)
+        u = try schema.decode(data: Data(hex: "0100")).parse(UInt16.self)
+        XCTAssertEqual(u, 1)
 
         schema = try TypeSchema(base64: TEST_CONTRACT_RECEIVE_ERROR_SCHEMA)
         let i = try schema.decode(data: Data(hex: "ffff")).parse(Int16.self)
         XCTAssertEqual(i, -1)
+    }
+
+    func testComplexTypeSchemaConvert() throws {
+        let schema = try TypeSchema(base64: AUCTION_WITH_ERRORS_VIEW_RETURN_VALUE_SCHEMA)
+        var json = """
+        {"auction_state":{"NotSoldYet":[]},"end":"2000-01-01T12:00:00+00:00","highest_bidder":{"None":[]},"item":"Test item"}
+        """
+        var value = try schema.encode(json: json)
+        var expected = try Data(hex: "00000900000054657374206974656d00da626ddc000000")
+        XCTAssertEqual(value, expected)
+        XCTAssertEqual(try schema.decode(data: expected).value, json)
+
+        json = """
+        {"auction_state":{"Sold":["35CJPZohio6Ztii2zy1AYzJKvuxbGG44wrBn7hLHiYLoF2nxnh"]},"end":"2000-01-01T12:00:00+00:00","highest_bidder":{"Some":["35CJPZohio6Ztii2zy1AYzJKvuxbGG44wrBn7hLHiYLoF2nxnh"]},"item":"Test item"}
+        """
+        value = try schema.encode(json: json)
+        expected = try Data(hex: "0110eac3f30aa2489508c86eb09328ff8a5475f9fe5c9458cc3c70956fcfcb22bf0110eac3f30aa2489508c86eb09328ff8a5475f9fe5c9458cc3c70956fcfcb22bf0900000054657374206974656d00da626ddc000000")
+        
+        XCTAssertEqual(value, expected)
+        XCTAssertEqual(try schema.decode(data: expected).value, json)
     }
 }
